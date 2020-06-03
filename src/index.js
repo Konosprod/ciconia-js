2469a5287f549e18eec7271340bb9c26c916b443
Konosprod <konosprod@free.fr>
2020-06-03 13:06:14 +0200

    Add config module
    Signed-off-by: Konosprod <konosprod@free.fr>

--- src/index.js
+++ src/index.js
@@ -7,9 +7,19 @@ const bodyparser = require("body-parser");
 const argon2 = require("argon2");
 const fs = require("fs-extra");
 const sharp = require("sharp");
+const path  = require("path");
+const config = require("config");
 
 const app = express();
 const port = 3000;
+const imageType = [
+    "image/jpeg",
+    "image/png",
+    "image/webp",
+    "image/bmp",
+    "image/gif"
+];
+
 
 var jsonparser = bodyparser.json();
 
@@ -20,21 +30,21 @@ var connection = mysql.createConnection({
     database: "ciconia"
 });
 
-var basename = "http://localhost:3000";
+var basename = config.get("baseurl");
 
 connection.connect(function(err) {
     if(err) {
         throw err;
     }
-})
+});
 
-var storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         var date = new Date();
         const year = date.getFullYear();
         const month = date.getMonth();
-        const userid = req.header("apikey")
-        const dest = `./uploads/${userid}/${year}/${month}`
+        const userid = req.header("apikey");
+        const dest = `./uploads/${userid}/${year}/${month}`;
 
         if (!fs.existsSync(dest))
         {
@@ -47,11 +57,11 @@ var storage = multer.diskStorage({
     filename: function(req, file, cb) {
         cb(null, file.originalname);
     }
-})
+});
 
 var upload = multer({
     storage: storage
-})
+});
 
 var authentication = function(req, res, next) {
     if(req.header("apikey") && req.header("username")) {
@@ -61,7 +71,7 @@ var authentication = function(req, res, next) {
         err.statusCode = 403;
         next(err);
     }
-}
+};
 
 function makeid(length) {
     var result = '';
@@ -73,15 +83,33 @@ function makeid(length) {
     }
     
     return result;
-}
+};
 
 function generateThumbs(req) {
-    console.log(req.file.mimetype);
-}
+    const optionsResize = {
+        fit: sharp.fit.outside
+    };
+
+    if(imageType.includes(req.file.mimetype)) {
+        var pathfile = path.join(path.dirname(req.file.path), "thumbs")
+
+        if(!fs.existsSync(path))
+            fs.mkdirpSync(pathfile);
+
+        var filename = path.basename(req.file.path);
+
+        sharp(req.file.path).resize(200, 200, optionsResize).toFile(path.join(pathfile, filename), (err) => {
+            if(err)
+                throw err;
+        });
+    } else {
+
+    }
+};
 
 app.post("/", authentication, upload.single("f"), (req, res, next) => {
     generateThumbs(req);
-    res.json({url:basename+"/push/" + makeid(8)})
+    res.json({url:basename+"/push/" + makeid(8)});
 });
 
 app.get("/push/$id", (req, res) => {
@@ -101,15 +129,17 @@ app.post("/register", jsonparser, function(req, res) {
                 username : req.body.username,
                 password : hash,
                 apikey : uuid.v4()
-            }
+            };
+
             connection.query("INSERT INTO users SET ?", user, function(err, res) {
                 if(err)
                     throw err;
-            }) 
+            });
+
             res.sendStatus(201);
         })
     });
 });
 
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`));
