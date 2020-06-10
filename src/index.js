ebcd9ac92e4fc5c685f6e1c700990dcaf3c29cbb
Konosprod <konosprod@free.fr>
2020-06-10 10:58:27 +0200

    Rename /push/thumbs to /thumbs and add thumbnail generation for other filetype than images
    Signed-off-by: Konosprod <konosprod@free.fr>

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const bodyparser = require("body-parser");
 const argon2 = require("argon2");
 const fs = require("fs-extra");
 const sharp = require("sharp");
-const path  = require("path");
+const path = require("path");
 const config = require("config");
 const winston = require("winston");
 const session = require("express-session");
@@ -28,9 +28,9 @@ const storeOptions = {
     schema: {
         tableName: 'sessions',
         columnNames: {
-            session_id : "session_id",
-            expires : "expires",
-            data: "data" 
+            session_id: "session_id",
+            expires: "expires",
+            data: "data"
         }
     },
     endConnectionOnClose: true,
@@ -48,7 +48,7 @@ const logFormat = winston.format.combine(
     winston.format.align(),
     winston.format.printf(({ level, message, label, timestamp }) => {
         return `${timestamp} [${label}] ${level}: ${message}`;
-      })
+    })
 );
 
 const logger = winston.createLogger({
@@ -58,8 +58,8 @@ const logger = winston.createLogger({
         label: 'ciconia-js'
     },
     transports: [
-        new winston.transports.File({filename: config.get("log.error_path"), level:"error"}),
-        new winston.transports.File({filename: config.get("log.combined_path")}),
+        new winston.transports.File({ filename: config.get("log.error_path"), level: "error" }),
+        new winston.transports.File({ filename: config.get("log.combined_path") }),
         new winston.transports.Console()
     ]
 })
@@ -74,8 +74,8 @@ const connection = mysql.createConnection({
 
 const basename = config.get("baseurl");
 
-connection.connect(function(err) {
-    if(err) {
+connection.connect(function (err) {
+    if (err) {
         logger.error(err)
         throw err;
     }
@@ -90,7 +90,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        httpOnly : false,
+        httpOnly: false,
         sameSite: true,
         secure: config.get("session.cookiesecure")
     },
@@ -99,23 +99,22 @@ app.use(session({
 }))
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: function (req, file, cb) {
         var date = new Date();
         const year = date.getFullYear();
         const month = date.getMonth();
         const userid = req.header("apikey");
         const dest = path.join(config.get("base_upload_directory"), userid, year.toString(), month.toString());
 
-        if (!fs.existsSync(dest))
-        {
+        if (!fs.existsSync(dest)) {
             fs.mkdirpSync(dest);
         }
 
         return cb(null, dest);
     },
 
-    filename: function(req, file, cb) {
-        
+    filename: function (req, file, cb) {
+
         let filename = file.originalname;
 
         const date = new Date();
@@ -129,7 +128,7 @@ const storage = multer.diskStorage({
         const extension = path.extname(file.originalname);
         const basefilename = path.basename(file.originalname, extension);
 
-        while(fs.existsSync(filepath)) {
+        while (fs.existsSync(filepath)) {
 
             filename = basefilename + "_" + nb.toString() + extension;
             filepath = path.join(config.get("base_upload_directory"), userid, year.toString(), month.toString(), filename)
@@ -144,37 +143,37 @@ const upload = multer({
     storage: storage
 });
 
-const authentication = function(req, res, next) {
+const authentication = function (req, res, next) {
 
     //Authenciate via headers
-    if(req.header("apikey") && req.header("username")) {
+    if (req.header("apikey") && req.header("username")) {
         let username = req.header("username")
         let apikey = req.header("apikey")
 
-        let query  = "SELECT apikey FROM users WHERE username = ?";
+        let query = "SELECT apikey FROM users WHERE username = ?";
 
-        connection.query(query, username, function(err, res) {
-            if(err) {
+        connection.query(query, username, function (err, res) {
+            if (err) {
                 logger.error(err);
                 throw err;
             }
             //If no user was found, or apikey mismatch, error
-            if(res.length <= 0 || res[0].apikey != apikey) {
+            if (res.length <= 0 || res[0].apikey != apikey) {
                 var err = new Error(`${req.ip} tried to access ${req.originalUrl} without being authenticated`)
                 err.statusCode = 403;
 
                 logger.warn(err);
-                
+
                 next(err);
             } else {
                 logger.info(`username : ${username} ; apikey : ${apikey} auth successful`)
                 next();
             }
         })
-      //Authenticate via session
+        //Authenticate via session
     } else if (req.session.hasOwnProperty("logged") && req.session.logged == true) {
         next();
-        
+
     } else {
         var err = new Error(`${req.ip} tried to access ${req.originalUrl} without being authenticated`)
         err.statusCode = 403;
@@ -188,8 +187,8 @@ const authentication = function(req, res, next) {
 function checkDatabase() {
     logger.debug("Checking database");
     var query = fs.readFileSync("./config/database.sql", "utf-8");
-    connection.query(query, function(err) {
-        if(err) {
+    connection.query(query, function (err) {
+        if (err) {
             logger.error(err);
             throw err;
         }
@@ -200,11 +199,11 @@ function makeid(length) {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const charactersLength = characters.length;
-    
+
     for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    
+
     return result;
 };
 
@@ -213,23 +212,41 @@ function generateThumbs(req) {
         fit: sharp.fit.outside
     };
 
-    if(imageType.includes(mimetype.lookup(req.file.path))) {
-        let pathfile = path.join(path.dirname(req.file.path), "thumbs")
+    var mime = mimetype.lookup(req.file.path);
+    var pathfile = path.join(path.dirname(req.file.path), "thumbs")
+    var filename = path.basename(req.file.path) + ".png";
 
-        if(!fs.existsSync(path))
+
+    if (imageType.includes(mime)) {
+
+        if (!fs.existsSync(path))
             fs.mkdirpSync(pathfile);
 
-        let filename = path.basename(req.file.path);
-        
         logger.info("Generating thumbnail for file : " + req.file.path);
 
         sharp(req.file.path).resize(config.get("thumbnail.x"), config.get("thumbnail.y"), optionsResize).toFile(path.join(pathfile, filename), (err) => {
-            if(err) {
+            if (err) {
                 logger.error(err);
                 throw err;
             }
         });
     } else {
+        //Default thumbnails
+
+        if (mime.startsWith("text/")) {
+            fs.copyFileSync("assets/thumb-text.png", path.join(pathfile, filename));
+        } else if (mime.startsWith("application/")) {
+            fs.copyFileSync("assets/thumb-bin.png", path.join(pathfile, filename));
+
+        } else if (mime.startsWith("audio/")) {
+            fs.copyFileSync("assets/thumb-audio.png", path.join(pathfile, filename));
+
+        } else if (mime.startsWith("video/")) {
+            fs.copyFileSync("assets/thumb-video.png", path.join(pathfile, filename));
+
+        } else {
+            fs.copyFileSync("assets/thumb-bin.png", path.join(pathfile, filename));
+        }
 
     }
 };
@@ -270,29 +287,29 @@ app.post("/", authentication, upload.single("f"), (req, res, next) => {
     });
 });
 
-app.get("/push/:thumbs?/:id(\\w{"+config.get("short_url_length")+"})/", (req, res, next) => {
+app.get("/push/:id(\\w{" + config.get("short_url_length") + "})/", (req, res, next) => {
     let id = req.params.id;
     let isthumbs = (req.params.hasOwnProperty("thumbs") && req.params.thumbs);
     let sql = "SELECT path FROM push WHERE url = ?";
 
     connection.query(sql, id, (err, result) => {
-        if(err) {
+        if (err) {
             logger.error(err);
             next(err);
             throw err;
         }
 
-        if(result.length <= 0) {
+        if (result.length <= 0) {
             logger.warn(`Push ${id} not found`);
             res.sendStatus(404);
         } else {
 
-            if(!isthumbs) {
+            if (!isthumbs) {
                 res.sendFile(result[0].path);
             } else {
                 let basepath = path.dirname(result[0].path);
-                let filename = path.basename(result[0].path);
-                let filepath = path.join(basepath, "thumbs", filename);
+                let filename = path.basename(result[0].path) + ".png";
+                let filepath = path.join(basepath, "thumbs", filename + "");
 
                 res.sendFile(filepath);
 
@@ -301,36 +318,60 @@ app.get("/push/:thumbs?/:id(\\w{"+config.get("short_url_length")+"})/", (req, re
     })
 });
 
-app.post("/register", jsonparser, function(req, res, next) {
+app.get("/thumbs/:id(\\w{" + config.get("short_url_length") + "})/", (req, res, next) => {
+    let id = req.params.id;
+    let sql = "SELECT path FROM push WHERE url = ?";
 
-    if(!req.body) {
-        logger.warn(`${req.ip} tried to register without sending json payload`)
-        return res.sendStatus(404);
-    }
-
-    crypto.randomBytes(32, function(err, salt) {
-        if(err) {
+    connection.query(sql, id, (err, result) => {
+        if (err) {
             logger.error(err);
             next(err);
             throw err;
         }
-        
+
+        if (result.length <= 0) {
+            logger.warn(`Push ${id} not found`);
+            res.sendStatus(404);
+        } else {
+            let basepath = path.dirname(result[0].path);
+            let filename = path.basename(result[0].path) + ".png";
+            let filepath = path.join(basepath, "thumbs", filename + "");
+
+            res.sendFile(filepath);
+        }
+    })
+});
+
+app.post("/register", jsonparser, function (req, res, next) {
+
+    if (!req.body) {
+        logger.warn(`${req.ip} tried to register without sending json payload`)
+        return res.sendStatus(404);
+    }
+
+    crypto.randomBytes(32, function (err, salt) {
+        if (err) {
+            logger.error(err);
+            next(err);
+            throw err;
+        }
+
         argon2.hash(req.body.password, salt).then(hash => {
             var user = {
-                username : req.body.username,
-                password : hash,
-                apikey : uuid.v4()
+                username: req.body.username,
+                password: hash,
+                apikey: uuid.v4()
             };
-            
-            connection.query("INSERT INTO users SET ?", user, function(err, res) {
-                if(err) {
+
+            connection.query("INSERT INTO users SET ?", user, function (err, res) {
+                if (err) {
                     logger.error(err);
                     next(err);
                     throw err;
                 }
             });
 
-            res.status(201).jsonjson({status: "ok"});
+            res.status(201).jsonjson({ status: "ok" });
 
             logger.info(`${req.ip} sucessfully registered : `);
             logger.debug(JSON.stringify(user));
@@ -339,31 +380,31 @@ app.post("/register", jsonparser, function(req, res, next) {
     });
 });
 
-app.post("/login", jsonparser, function(req, res, next) {
+app.post("/login", jsonparser, function (req, res, next) {
     let username = req.body.username;
     let password = req.body.password;
     let sql = "SELECT password, id FROM users WHERE username = ?";
 
-    connection.query(sql, username, function(err, results) {
-        if(err) {
+    connection.query(sql, username, function (err, results) {
+        if (err) {
             logger.error(err);
             next(err);
             throw err;
         }
 
-        if(results.length <= 0) {
+        if (results.length <= 0) {
             logger.warn(`User : ${username} not found !`);
-            res.statusCode(403).json({status:"error", message:"Invalid user or password"})
+            res.statusCode(403).json({ status: "error", message: "Invalid user or password" })
         } else {
             argon2.verify(results[0].password, password).then(result => {
-                if(result) {
+                if (result) {
                     req.session.logged = true;
                     req.session.uid = results[0].id;
-                    res.json({status:"ok"});
+                    res.json({ status: "ok" });
                     next();
-                } else  {
+                } else {
                     logger.warn(`User : ${username} password mismatch`);
-                    res.statusCode(403).json({status:"error", message:"Invalid user or password"})
+                    res.statusCode(403).json({ status: "error", message: "Invalid user or password" })
                 }
             }).catch(err => {
                 logger.error(err);
@@ -374,9 +415,9 @@ app.post("/login", jsonparser, function(req, res, next) {
 
 })
 
-app.get("/logout", jsonparser, function(req, res, next) {
+app.get("/logout", jsonparser, function (req, res, next) {
     req.session.destroy((err) => {
-        if(err) {
+        if (err) {
             logger.error(err);
             next(err);
             throw err;
@@ -386,19 +427,19 @@ app.get("/logout", jsonparser, function(req, res, next) {
     })
 })
 
-app.post("/gallery", authentication, jsonparser, function(req, res, next) {
+app.post("/gallery", authentication, jsonparser, function (req, res, next) {
 
-    if(!req.body.hasOwnProperty("limit") || !req.body.hasOwnProperty("offset") || !req.session.hasOwnProperty("uid")) {
+    if (!req.body.hasOwnProperty("limit") || !req.body.hasOwnProperty("offset") || !req.session.hasOwnProperty("uid")) {
         logger.warn("Bad request");
         res.sendStatus(400);
     } else {
         let sql = mysql.format("SELECT url, mime FROM push WHERE owner = ? ORDER BY id DESC LIMIT ? OFFSET ?", [req.session.uid, req.body.limit, req.body.offset]);
 
-        connection.query(sql, function(err, result) {
-            if(err) {
+        connection.query(sql, function (err, result) {
+            if (err) {
                 logger.error(err);
                 next(err);
-                throw(err);
+                throw (err);
             } else {
                 res.json(result);
             }
@@ -406,9 +447,9 @@ app.post("/gallery", authentication, jsonparser, function(req, res, next) {
     }
 })
 
-app.get('*', function(req, res){
+app.get('*', function (req, res) {
     res.status(404).send('what???');
-  });
+});
 
 app.listen(port, () => {
     logger.info(`Listening on port ${port}`)
